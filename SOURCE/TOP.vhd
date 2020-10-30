28f96ce5bd4e26290902a961c5ba51d0f938639c
marosm <marosm@DESKTOP-OUS1QF9.kn.vutbr.cz>
2020-10-30 01:35:05 +0100

    TOP template

--- SOURCE/TOP.vhd
+++ SOURCE/TOP.vhd
@@ -30,21 +30,22 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity TOP is
-    Port ( clk : in  STD_LOGIC;
-           ps2_clock_pin : inout  STD_LOGIC;
-           ps2_data_pin : inout  STD_LOGIC;
-           uart_tx : out  STD_LOGIC;
-           uart_rx : in  STD_LOGIC;
-           vga_R, vga_G, vga_B : out  STD_LOGIC_VECTOR (6 downto 0);
-           vga_VS, vga_HS : out  STD_LOGIC;
-           audio_out : out  STD_LOGIC_VECTOR (7 downto 0);
-           buzzer : out  STD_LOGIC);
+    Port(clk                 : in    STD_LOGIC;
+         ps2_clock_pin       : inout STD_LOGIC;
+         ps2_data_pin        : inout STD_LOGIC;
+         uart_tx             : out   STD_LOGIC;
+         uart_rx             : in    STD_LOGIC;
+         vga_R, vga_G, vga_B : out   STD_LOGIC_VECTOR(6 downto 0);
+         vga_VS, vga_HS      : out   STD_LOGIC;
+         audio_out           : out   STD_LOGIC_VECTOR(7 downto 0);
+         buzzer              : out   STD_LOGIC);
 end TOP;
 
 architecture Behavioral of TOP is
 
 begin
 
+    clk <= ps2_clock_pin;               -- LOOOOOOOOOOOOOOOL
 
 end Behavioral;
 
