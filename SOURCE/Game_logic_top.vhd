06c9e80576552cab282e8d82819c44a383c7a99c
Peniaze <42100202+Peniaze@users.noreply.github.com>
2020-11-09 13:08:06 +0100

    formatting

--- SOURCE/Game_logic_top.vhd
+++ SOURCE/Game_logic_top.vhd
@@ -30,89 +30,89 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity Game_logic_top is
-    Port ( pos_x : in  STD_LOGIC_VECTOR (10 downto 0);
-           pos_y : in  STD_LOGIC_VECTOR (9 downto 0);
-           button_r, button_l, scroll_up, scroll_down : in  STD_LOGIC;
-           clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           turn : in  STD_LOGIC;
-           miss_in, hit_in : in  STD_LOGIC;
-           game_type_real : in  STD_LOGIC;
-           shoot_position_in : in  STD_LOGIC_VECTOR (8 downto 0);
-           shoot_position_out : out  STD_LOGIC_VECTOR (8 downto 0);
-           hit_out, miss_out : out  STD_LOGIC;
-           game_type_want : out  STD_LOGIC;
-           re_A, we_A : out STD_LOGIC;
-           data_read_ram : in STD_LOGIC_VECTOR (8 downto 0);
-           data_write_ram : out STD_LOGIC_VECTOR (8 downto 0);
-           addr_A : out STD_LOGIC_VECTOR (10 downto 0));
+	Port(pos_x                                      : in  STD_LOGIC_VECTOR(10 downto 0);
+	     pos_y                                      : in  STD_LOGIC_VECTOR(9 downto 0);
+	     button_r, button_l, scroll_up, scroll_down : in  STD_LOGIC;
+	     clk                                        : in  STD_LOGIC;
+	     rst                                        : in  STD_LOGIC;
+	     turn                                       : in  STD_LOGIC;
+	     miss_in, hit_in                            : in  STD_LOGIC;
+	     game_type_real                             : in  STD_LOGIC;
+	     shoot_position_in                          : in  STD_LOGIC_VECTOR(8 downto 0);
+	     shoot_position_out                         : out STD_LOGIC_VECTOR(8 downto 0);
+	     hit_out, miss_out                          : out STD_LOGIC;
+	     game_type_want                             : out STD_LOGIC;
+	     re_A, we_A                                 : out STD_LOGIC;
+	     data_read_ram                              : in  STD_LOGIC_VECTOR(8 downto 0);
+	     data_write_ram                             : out STD_LOGIC_VECTOR(8 downto 0);
+	     addr_A                                     : out STD_LOGIC_VECTOR(10 downto 0));
+
 end Game_logic_top;
 
 architecture Behavioral of Game_logic_top is
 
-constant quick_game_left_boundary_x : natural := 200;
-constant quick_game_left_boundary_y : natural := 200;
-constant quick_game_right_boundary_x : natural := 400;
-constant quick_game_right_boundary_y : natural := 400;
+	constant quick_game_left_boundary_x  : natural := 200;
+	constant quick_game_left_boundary_y  : natural := 200;
+	constant quick_game_right_boundary_x : natural := 400;
+	constant quick_game_right_boundary_y : natural := 400;
 
-constant normal_game_left_boundary_x : natural := 400;
-constant normal_game_left_boundary_y : natural := 200;
-constant normal_game_right_boundary_x : natural := 600;
-constant normal_game_right_boundary_y : natural := 400;
+	constant normal_game_left_boundary_x  : natural := 400;
+	constant normal_game_left_boundary_y  : natural := 200;
+	constant normal_game_right_boundary_x : natural := 600;
+	constant normal_game_right_boundary_y : natural := 400;
 
-
-
-type stav is (init, start, placement, wait_4_player, my_turn, his_turn, ask,
-				hit_1_anim, miss_1_anim, hit_2_anim, miss_2_anim, game_over_win, game_over_lose);
-signal game_state, game_state_n : stav := init;
+	type stav is (init, start, placement, wait_4_player, my_turn, his_turn, ask,
+	              hit_1_anim, miss_1_anim, hit_2_anim, miss_2_anim, game_over_win, game_over_lose);
+	signal game_state, game_state_n : stav := init;
 
 begin
-process(clk, rst)
-begin
-	if (rst = '1') then
-		game_state <= init;
-	elsif (rising_edge(clk)) then
-		game_state <= game_state_n;
-	end if;
-end process;
-
-
-
-
-process(button_l, game_state, pos_x, pos_y)
-begin
-	game_state_n <= game_state;
-	case (game_state) is
-	when init =>
-		game_state_n <= start;
-	when start =>
-		if (button_l = '1') then
-			if ((unsigned(pos_x) > quick_game_left_boundary_x) and (unsigned(pos_x) < quick_game_right_boundary_x)
-			and (unsigned(pos_y) > quick_game_left_boundary_y) and (unsigned(pos_y) < quick_game_right_boundary_y))
-			then
-				game_state_n <= placement;
-				game_type_want <= '1';
-			elsif ((unsigned(pos_x) > normal_game_left_boundary_x) and (unsigned(pos_x) < normal_game_right_boundary_x)
-			and    (unsigned(pos_y) > normal_game_left_boundary_y) and (unsigned(pos_y) < normal_game_right_boundary_y))
-			then
-				game_state_n <= placement;
-				game_type_want <= '0';
-			end if;
+	process(clk, rst)
+	begin
+		if (rst = '1') then
+			game_state <= init;
+		elsif (rising_edge(clk)) then
+			game_state <= game_state_n;
 		end if;
-	when placement =>
-	when wait_4_player =>
-	when my_turn =>
-	when his_turn =>
-	when ask =>
-	when hit_1_anim =>
-	when miss_1_anim =>
-	when hit_2_anim =>
-	when miss_2_anim =>
-	when game_over_win =>
-	when game_over_lose =>
-	when others =>
-	end case;
-end process;
+	end process;
+
+	process(button_l, game_state, pos_x, pos_y)
+	begin
+		game_state_n <= game_state;
+		case (game_state) is
+			when init =>
+				game_state_n <= start;
+			when start =>
+				if (button_l = '1') then
+					if		((unsigned(pos_x) > quick_game_left_boundary_x)
+						and (unsigned(pos_x) < quick_game_right_boundary_x)
+						and (unsigned(pos_y) > quick_game_left_boundary_y)
+						and (unsigned(pos_y) < quick_game_right_boundary_y))
+					then
+						game_state_n   <= placement;
+						game_type_want <= '1';
+					elsif	((unsigned(pos_x) > normal_game_left_boundary_x)
+						and (unsigned(pos_x) < normal_game_right_boundary_x)
+						and (unsigned(pos_y) > normal_game_left_boundary_y)
+						and (unsigned(pos_y) < normal_game_right_boundary_y))
+					 then
+						game_state_n   <= placement;
+						game_type_want <= '0';
+					end if;
+				end if;
+			when placement =>
+			when wait_4_player =>
+			when my_turn =>
+			when his_turn =>
+			when ask =>
+			when hit_1_anim =>
+			when miss_1_anim =>
+			when hit_2_anim =>
+			when miss_2_anim =>
+			when game_over_win =>
+			when game_over_lose =>
+			when others =>
+		end case;
+	end process;
 
 
 end Behavioral;
--- SOURCE/RAM_2port.vhd
+++ SOURCE/RAM_2port.vhd
@@ -3,42 +3,41 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity RAM_2port is
-    port(
-        clk_GL, clk_VGA : in  STD_LOGIC;
-        we_GL              : in  STD_LOGIC;
-        re_GL		: in STD_LOGIC;
-        addr_GL         : in  STD_LOGIC_VECTOR(10 downto 0);
-        addr_VGA        : in  STD_LOGIC_VECTOR(10 downto 0);
-        data_in_GL         : in  STD_LOGIC_VECTOR(8 downto 0);
-        data_out_GL     : out STD_LOGIC_VECTOR(8 downto 0);
-        data_out_VGA    : out STD_LOGIC_VECTOR(8 downto 0)
-    );
+	port(
+		clk_GL, clk_VGA : in  STD_LOGIC;
+		we_GL           : in  STD_LOGIC;
+		re_GL           : in  STD_LOGIC;
+		addr_GL         : in  STD_LOGIC_VECTOR(10 downto 0);
+		addr_VGA        : in  STD_LOGIC_VECTOR(10 downto 0);
+		data_in_GL      : in  STD_LOGIC_VECTOR(8 downto 0);
+		data_out_GL     : out STD_LOGIC_VECTOR(8 downto 0);
+		data_out_VGA    : out STD_LOGIC_VECTOR(8 downto 0)
+	);
 end RAM_2port;
 
 architecture Behavioral of RAM_2port is
 
-    type RAM_t is array (0 to 2047) of std_logic_vector(8 downto 0);
-    signal ram : RAM_t;
+	type RAM_t is array (0 to 2047) of std_logic_vector(8 downto 0);
+	signal ram : RAM_t;
 
 begin
+	process(clk_GL)
+	begin
+		if (rising_edge(clk_GL)) then
+			if (we_GL = '1') then
+				ram(to_integer(unsigned(addr_GL))) <= data_in_GL;
+			elsif (re_GL = '1') then
+				data_out_GL <= ram(to_integer(unsigned(addr_GL)));
+			end if;
+		end if;
+	end process;
 
-    process(clk_GL)
-    begin
-        if (rising_edge(clk_GL)) then
-            if (we_GL = '1') then
-                ram(to_integer(unsigned(addr_GL))) <= data_in_GL;
-            elsif (re_GL = '1') then
-            	data_out_GL <= ram(to_integer(unsigned(addr_GL)));
-            end if;
-        end if;
-    end process;
-
-    process(clk_VGA)
-    begin
-    	if (rising_edge(clk_VGA)) then
-    		data_out_VGA <= ram(to_integer(unsigned(addr_VGA)));
-        end if;
-    end process;
+	process(clk_VGA)
+	begin
+		if (rising_edge(clk_VGA)) then
+			data_out_VGA <= ram(to_integer(unsigned(addr_VGA)));
+		end if;
+	end process;
 
 end Behavioral;
 
