39a9c617f6d51b5c31637a85c52158ed7d6703aa
Peniaze <42100202+Peniaze@users.noreply.github.com>
2020-11-09 11:21:40 +0100

    game_logic_top
    added game_logic_top
    refactor of 2port ram & added read_enable

--- SOURCE/RAM_2port.vhd
+++ SOURCE/RAM_2port.vhd
@@ -5,10 +5,11 @@ use IEEE.NUMERIC_STD.ALL;
 entity RAM_2port is
     port(
         clk_GL, clk_VGA : in  STD_LOGIC;
-        we              : in  STD_LOGIC;
+        we_GL              : in  STD_LOGIC;
+        re_GL		: in STD_LOGIC;
         addr_GL         : in  STD_LOGIC_VECTOR(10 downto 0);
         addr_VGA        : in  STD_LOGIC_VECTOR(10 downto 0);
-        data_in         : in  STD_LOGIC_VECTOR(8 downto 0);
+        data_in_GL         : in  STD_LOGIC_VECTOR(8 downto 0);
         data_out_GL     : out STD_LOGIC_VECTOR(8 downto 0);
         data_out_VGA    : out STD_LOGIC_VECTOR(8 downto 0)
     );
@@ -24,17 +25,18 @@ begin
     process(clk_GL)
     begin
         if (rising_edge(clk_GL)) then
-            if (we = '1') then
-                ram(to_integer(unsigned(addr_GL))) <= data_in;
+            if (we_GL = '1') then
+                ram(to_integer(unsigned(addr_GL))) <= data_in_GL;
+            elsif (re_GL = '1') then
+            	data_out_GL <= ram(to_integer(unsigned(addr_GL)));
             end if;
-            data_out_GL <= ram(to_integer(unsigned(addr_GL)));
         end if;
     end process;
 
     process(clk_VGA)
     begin
-        if (rising_edge(clk_VGA)) then
-            data_out_VGA <= ram(to_integer(unsigned(addr_VGA)));
+    	if (rising_edge(clk_VGA)) then
+    		data_out_VGA <= ram(to_integer(unsigned(addr_VGA)));
         end if;
     end process;
 
