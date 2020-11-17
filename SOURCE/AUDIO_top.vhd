f64145f55767e10c17c1a827b982583a3c91845c
MarosMacko <mackomarosh@gmail.com>
2020-11-17 18:19:22 +0100

    Defaults needed for Translation

--- SOURCE/AUDIO_top.vhd
+++ SOURCE/AUDIO_top.vhd
@@ -3,18 +3,20 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Audio_top is
-	port(clk, rst   : in  std_logic;
-	     sound_type : in  STD_LOGIC_VECTOR(1 downto 0);
-	     audio_out  : out STD_LOGIC_VECTOR(7 downto 0)
-	    );
-end entity Audio_top;
+    port(clk, rst   : in  std_logic;
+         sound_type : in  STD_LOGIC_VECTOR(1 downto 0);
+         audio_out  : out STD_LOGIC_VECTOR(7 downto 0)
+        );
+end Audio_top;
 
 architecture Behavioral of Audio_top is
 
-	constant miss_sound  : integer := 1;
-	constant hit_sound   : integer := 2;
-	constant shoot_sound : integer := 3;
+    constant miss_sound  : integer := 1;
+    constant hit_sound   : integer := 2;
+    constant shoot_sound : integer := 3;
 
 begin
+
+    audio_out <= (others => '0');
 
 end architecture Behavioral;
--- SOURCE/MultiPlayer_top.vhd
+++ SOURCE/MultiPlayer_top.vhd
@@ -4,30 +4,38 @@ use ieee.numeric_std.all;
 
 entity MultiPlayer_top is
 
-	generic(data_width : integer := 8); -- data bus width --
+    generic(data_width : integer := 8); -- data bus width --
 
-	port(clk, rst           : in  std_logic;
-	     -----------------------------------------	-- signals between MP_LOGIC & UART --
-	     tx_data            : out std_logic_vector(data_width - 1 downto 0);
-	     tx_send_CE         : out std_logic;
-	     tx_busy            : in  std_logic;
-	     rx_data            : in  std_logic_vector(data_width - 1 downto 0);
-	     rx_receive_CE      : in  std_logic;
-	     ----------------------------------------- -- signals between MP_LOGIC & GAME_LOGIC --
-	     turn               : out std_logic;
-	     game_type_want     : in  std_logic;
-	     game_type_real     : out std_logic;
-	     miss_in            : out std_logic;
-	     hit_in             : out std_logic;
-	     miss_out           : in  std_logic;
-	     hit_out            : in  std_logic;
-	     shoot_position_out : in  std_logic_vector(8 downto 0);
-	     shoot_position_in  : out std_logic_vector(8 downto 0)
-	    );
+    port(clk, rst           : in  std_logic;
+         -----------------------------------------	-- signals between MP_LOGIC & UART --
+         tx_data            : out std_logic_vector(data_width - 1 downto 0);
+         tx_send_CE         : out std_logic;
+         tx_busy            : in  std_logic;
+         rx_data            : in  std_logic_vector(data_width - 1 downto 0);
+         rx_receive_CE      : in  std_logic;
+         ----------------------------------------- -- signals between MP_LOGIC & GAME_LOGIC --
+         turn               : out std_logic;
+         game_type_want     : in  std_logic;
+         game_type_real     : out std_logic;
+         miss_in            : out std_logic;
+         hit_in             : out std_logic;
+         miss_out           : in  std_logic;
+         hit_out            : in  std_logic;
+         shoot_position_out : in  std_logic_vector(8 downto 0);
+         shoot_position_in  : out std_logic_vector(8 downto 0)
+        );
 end entity MultiPlayer_top;
 
 architecture RTL of MultiPlayer_top is
 
 begin
+
+    tx_data           <= (others => '0');
+    tx_send_CE        <= '0';
+    turn              <= '0';
+    game_type_real    <= '0';
+    miss_in           <= '0';
+    hit_in            <= '0';
+    shoot_position_in <= (others => '0');
 
 end architecture RTL;
