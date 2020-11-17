eda59a190b26de19d35d83426a4727be0389645c
MarosMacko <mackomarosh@gmail.com>
2020-11-17 21:24:27 +0100

    Fixed entity and arch name

--- SOURCE/VGA_hud_gen.vhd
+++ SOURCE/VGA_hud_gen.vhd
@@ -2,16 +2,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity VGA_pixel_gen is
+entity VGA_hud_gen is
     Port(clk        : in  STD_LOGIC;
          pixel_x    : in  STD_LOGIC_VECTOR(10 downto 0);
          pixel_y    : in  STD_LOGIC_VECTOR(10 downto 0);
          frame_tick : in  STD_LOGIC;
-         HUD_on     : out STD_LOGIC;
+         HUD_on     : out STD_LOGIC := '0';
          R, G, B    : out STD_LOGIC_VECTOR(6 downto 0));
-end VGA_pixel_gen;
+end VGA_hud_gen;
 
-architecture RTL of VGA_pixel_gen is
+architecture RTL of VGA_hud_gen is
 
 begin
 
