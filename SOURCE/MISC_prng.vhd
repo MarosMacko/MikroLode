e3837688aed593694fa45b33106c405f86410968
MarosMacko <mackomarosh@gmail.com>
2021-01-31 12:14:59 +0100

    RNG edits

--- SOURCE/MISC_prng.vhd
+++ SOURCE/MISC_prng.vhd
@@ -3,28 +3,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity MISC_prng is
-    Port(clk, rst      : in  STD_LOGIC;
-         random_output : out STD_LOGIC_VECTOR(15 downto 0));
+    Port(clk_rng, clk_sync, rst : in  STD_LOGIC;
+         random_output          : out STD_LOGIC_VECTOR(15 downto 0));
 end MISC_prng;
 
 architecture Behavioral of MISC_prng is
-    signal counter, counter_n : std_logic_vector(15 downto 0) := (others => '0');
+    signal counter, counter_n, out_prep, out_prep_n, out_meta, out_stable : std_logic_vector(15 downto 0) := (others => '0');
+
+    attribute keep : boolean;
+    --attribute keep of out_prep : signal is true;
+    attribute keep of out_meta : signal is true;
+    attribute keep of out_stable : signal is true;
 begin
 
-    rng_seq : process(clk, rst)
+    rng_seq : process(clk_rng, rst)
     begin
         if (rst = '1') then
-            counter <= (others => '0');
-        elsif (rising_edge(clk)) then
-            counter <= counter_n;
+            counter  <= (others => '0');
+            out_prep <= (others => '0');
+        elsif (rising_edge(clk_rng)) then
+            counter  <= counter_n;
+            out_prep <= out_prep_n;
         end if;
     end process;
 
     comb : process(counter)
     begin
         counter_n <= std_logic_vector(unsigned(counter) + 1);
+        if (counter(0) = '0') then
+            out_prep_n <= (others => '0');
+        else
+            out_prep_n <= "0" & counter(15 downto 1);
+        end if;
     end process;
-        
-    random_output <= (others => '0') when counter(0) = '0' else "0" & counter (15 downto 1);
+
+    output_buf : process(clk_sync)
+    begin
+        if (rising_edge(clk_sync)) then
+            out_meta   <= out_prep;
+            out_stable <= out_meta;
+        end if;
+
+    end process;
+
+    random_output <= out_stable;
 
 end Behavioral;
