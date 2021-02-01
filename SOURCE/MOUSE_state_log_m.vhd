8f1c64d5af06407710f9c2f26e1c39b570e9b294
Zach- <Zach-@147.229.220.216>
2021-02-01 22:31:27 +0100

    "pretec" signaly jsou pryč

--- SOURCE/MOUSE_state_log_m.vhd
+++ SOURCE/MOUSE_state_log_m.vhd
@@ -8,160 +8,146 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity state_log_m is
-    Port(clk, rst                     : in  STD_LOGIC;
-         tx_done_en, rx_done_en       : in  STD_LOGIC;
-         rx_data                      : in  STD_LOGIC_VECTOR(7 downto 0);
-         tx_en, rx_en, CE_rdy                 : out STD_LOGIC;
-         num_to_mouse                 : out STD_LOGIC_VECTOR(7 downto 0);
+    Port(clk, rst                        : in  STD_LOGIC;
+         tx_done_en, rx_done_en          : in  STD_LOGIC;
+         rx_data                         : in  STD_LOGIC_VECTOR(7 downto 0);
+         tx_en, rx_en, CE_rdy            : out STD_LOGIC;
+         num_to_mouse                    : out STD_LOGIC_VECTOR(7 downto 0);
          btn_l_out, btn_r_out, btn_m_out : out STD_LOGIC;
-			x_pretec_out, y_pretec_out		: out STD_LOGIC;
-         x_data_out                   : out STD_LOGIC_VECTOR(8 downto 0);
-         y_data_out                   : out STD_LOGIC_VECTOR(8 downto 0));
+         x_data_out                      : out STD_LOGIC_VECTOR(8 downto 0);
+         y_data_out                      : out STD_LOGIC_VECTOR(8 downto 0));
 end state_log_m;
 
 architecture Behavioral of state_log_m is
     type state_type is (idle_rst, ack_ff, slf_test, id_mouse, stream_md, pack1, pack2, pack3, ack_ff2, decod_x, decod_y, done);
     signal next_state, present_state : state_type;
 
-    signal btn_l_next, btn_l         : std_logic;
-    signal btn_r_next, btn_r         : std_logic;
-    signal btn_m_next, btn_m         : std_logic;
-    signal x_data_next, x_data       : std_logic_vector(8 downto 0);
-    signal y_data_next, y_data       : std_logic_vector(8 downto 0);
-    signal x_pretec_next, x_pretec   : std_logic;
-    signal y_pretec_next, y_pretec   : std_logic;
-
+    signal btn_l_next, btn_l   : std_logic;
+    signal btn_r_next, btn_r   : std_logic;
+    signal btn_m_next, btn_m   : std_logic;
+    signal x_data_next, x_data : std_logic_vector(8 downto 0);
+    signal y_data_next, y_data : std_logic_vector(8 downto 0);
 
 begin
     --hlavny stavovy automat
     --sekvencna cast:
-	process (clk,rst)
-	begin if rst='1' then
-				present_state <= idle_rst;
-				btn_l_next <= '0';
-				btn_r_next <= '0';
-				btn_m_next <= '0';
-				x_data_next <= (others => '0');
-				y_data_next <= (others => '0');
-				x_pretec_next <= '0';
-				y_pretec_next <= '0';
-			elsif (clk'event and clk='1') then
-				present_state <= next_state;
-				btn_l_next <= btn_l;
-				btn_r_next <= btn_r;
-				btn_m_next <= btn_m;
-				x_data_next <= x_data;
-				y_data_next <= y_data;
-				x_pretec_next <= x_pretec;
-				y_pretec_next <= y_pretec;
-			end if;
-	end process;
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            present_state <= idle_rst;
+            btn_l_next    <= '0';
+            btn_r_next    <= '0';
+            btn_m_next    <= '0';
+            x_data_next   <= (others => '0');
+            y_data_next   <= (others => '0');
+        elsif (clk'event and clk = '1') then
+            present_state <= next_state;
+            btn_l_next    <= btn_l;
+            btn_r_next    <= btn_r;
+            btn_m_next    <= btn_m;
+            x_data_next   <= x_data;
+            y_data_next   <= y_data;
+        end if;
+    end process;
 
---kombinacna_cast:
-	process(present_state,tx_done_en,rx_done_en,rx_data,btn_l_next, btn_r_next,btn_m_next,x_data_next,y_data_next,x_pretec_next,
-				y_pretec_next)
-	begin
-		next_state <= present_state;
-		tx_en <= '0';
-		rx_en <= '0';
-		num_to_mouse <= (others => '0');		
-		btn_l <= btn_l_next;
-		btn_r <= btn_r_next;
-		btn_m <= btn_m_next;
-		x_data <= x_data_next;
-		y_data <= y_data_next;
-		x_pretec <= x_pretec_next;
-		y_pretec <= y_pretec_next;
-		CE_rdy <= '0';
-		case present_state is
-			when idle_rst =>
-				tx_en <= '1';
-				num_to_mouse <= "11111111";			--poslat FF pre reset mouse
-					if (tx_done_en = '1') then							
-						next_state <= ack_ff;
-					end if;
-					
-			when ack_ff =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						if (rx_data = "11111010") then						--ak odpoved je ack cize FA
-							next_state <= slf_test;
-						end if;
-					end if;
-					
-			when slf_test =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						if (rx_data = "10101010") then			--Self-test passed cize posle AA
-							next_state <= id_mouse;
-						end if;
-					end if;
-			
-			when id_mouse =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						if (rx_data = "00000000") then			-- mouse device id - 00 
-							next_state <= stream_md;
-						end if;
-					end if;
-					
-			when stream_md =>
-				tx_en <= '1';
-				num_to_mouse <= "11110100";			--poslat FE to enable stream mode
-					if (tx_done_en = '1') then							
-						next_state <= ack_ff2;
-					end if;
-				
-			when ack_ff2 =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						if (rx_data = "11111010") then			--ak odpoved je ack cize FA
-							next_state <= pack1;
-						end if;
-					end if;
-					
-			when pack1 =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						btn_l <= rx_data(0);
-						btn_r <= rx_data(1);
-						btn_m <= rx_data(2);
-						x_data(8) <= rx_data(4);
-						y_data(8) <= rx_data(5);
-						x_pretec <= rx_data(6);
-						y_pretec <= rx_data(7);
-						next_state <= pack2;
-					end if;
-					
-			when pack2 =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						x_data(7 downto 0) <= rx_data;
-						next_state <= pack3;
-					end if;
-			
-			when pack3 =>
-				rx_en <= '1';
-					if (rx_done_en = '1') then	
-						y_data(7 downto 0) <= rx_data;
-						next_state <= done;
-					end if;
-				
-			when done =>
-				CE_rdy <= '1';
-				next_state <= pack1;
-							
-			when others =>
-				next_state <= idle_rst;
-		end case;
-	end process;
-	
-x_data_out <= x_data;
-y_data_out <= y_data;
-btn_l_out <= btn_l;
-btn_r_out <= btn_r;
-btn_m_out <= btn_m;
-x_pretec_out <= x_pretec;
-y_pretec_out <= y_pretec;
+    --kombinacna_cast:
+    process(present_state, tx_done_en, rx_done_en, rx_data, btn_l_next, btn_r_next, btn_m_next, x_data_next, y_data_next)
+    begin
+        next_state   <= present_state;
+        tx_en        <= '0';
+        rx_en        <= '0';
+        num_to_mouse <= (others => '0');
+        btn_l        <= btn_l_next;
+        btn_r        <= btn_r_next;
+        btn_m        <= btn_m_next;
+        x_data       <= x_data_next;
+        y_data       <= y_data_next;
+        CE_rdy       <= '0';
+        case present_state is
+            when idle_rst =>
+                tx_en        <= '1';
+                num_to_mouse <= "11111111"; --poslat FF pre reset mouse
+                if (tx_done_en = '1') then
+                    next_state <= ack_ff;
+                end if;
+
+            when ack_ff =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    if (rx_data = "11111010") then --ak odpoved je ack cize FA
+                        next_state <= slf_test;
+                    end if;
+                end if;
+
+            when slf_test =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    if (rx_data = "10101010") then --Self-test passed cize posle AA
+                        next_state <= id_mouse;
+                    end if;
+                end if;
+
+            when id_mouse =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    if (rx_data = "00000000") then -- mouse device id - 00 
+                        next_state <= stream_md;
+                    end if;
+                end if;
+
+            when stream_md =>
+                tx_en        <= '1';
+                num_to_mouse <= "11110100"; --poslat FE to enable stream mode
+                if (tx_done_en = '1') then
+                    next_state <= ack_ff2;
+                end if;
+
+            when ack_ff2 =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    if (rx_data = "11111010") then --ak odpoved je ack cize FA
+                        next_state <= pack1;
+                    end if;
+                end if;
+
+            when pack1 =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    btn_l      <= rx_data(0);
+                    btn_r      <= rx_data(1);
+                    btn_m      <= rx_data(2);
+                    x_data(8)  <= rx_data(4);
+                    y_data(8)  <= rx_data(5);
+                    next_state <= pack2;
+                end if;
+
+            when pack2 =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    x_data(7 downto 0) <= rx_data;
+                    next_state         <= pack3;
+                end if;
+
+            when pack3 =>
+                rx_en <= '1';
+                if (rx_done_en = '1') then
+                    y_data(7 downto 0) <= rx_data;
+                    next_state         <= done;
+                end if;
+
+            when done =>
+                CE_rdy     <= '1';
+                next_state <= pack1;
+
+            when others =>
+                next_state <= idle_rst;
+        end case;
+    end process;
+
+    x_data_out <= x_data;
+    y_data_out <= y_data;
+    btn_l_out  <= btn_l;
+    btn_r_out  <= btn_r;
+    btn_m_out  <= btn_m;
 
 end Behavioral;
--- SOURCE/MOUSE_top.vhd
+++ SOURCE/MOUSE_top.vhd
@@ -24,27 +24,24 @@ architecture Behavioral of MOUSE_top is
     signal IN_SIG_dat, OUT_SIG_dat, T_ENABLE_dat : std_logic;
 
     --pomocne signaly
-    signal ps2c_deb     : std_logic;    --vystup z debouncra pre ps2c
-    signal ps2c_fall    : std_logic;    --detekovana sestupna hrana
-    signal rx_en        : std_logic;    --enable pre citanie
-    signal rx_done_en   : std_logic;    --uspesne nacitanie dat z mouse
-    signal tx_en        : std_logic;    --enable pre zapis
-    signal tx_done_en   : std_logic;    --uspesne odoslanie to mouse
-	 signal CE_rdy: std_logic;	
-    signal num_to_mouse : std_logic_vector(7 downto 0); -- znak posielany do mouse
-    signal rx_data      : std_logic_vector(7 downto 0); -- precitane data z mouse
-	 signal plus_x, plus_y : std_logic_vector (10 downto 0);
-	 
-	 signal btn_l, btn_l_pom : std_logic;
-	 signal btn_r, btn_r_pom: std_logic;
-	 signal btn_m, btn_m_pom: std_logic;
-	 signal x_data: std_logic_vector (8 downto 0);			
-	 signal y_data: std_logic_vector (8 downto 0);
-	 signal x_pretec: std_logic;		
-	 signal y_pretec: std_logic;	
-	 signal pos_x_next, pos_x, pos_x_pom: std_logic_vector (10 downto 0);			
-	 signal pos_y_next, pos_y, pos_y_pom: std_logic_vector (10 downto 0);
-		
+    signal ps2c_deb       : std_logic;  --vystup z debouncra pre ps2c
+    signal ps2c_fall      : std_logic;  --detekovana sestupna hrana
+    signal rx_en          : std_logic;  --enable pre citanie
+    signal rx_done_en     : std_logic;  --uspesne nacitanie dat z mouse
+    signal tx_en          : std_logic;  --enable pre zapis
+    signal tx_done_en     : std_logic;  --uspesne odoslanie to mouse
+    signal CE_rdy         : std_logic;
+    signal num_to_mouse   : std_logic_vector(7 downto 0); -- znak posielany do mouse
+    signal rx_data        : std_logic_vector(7 downto 0); -- precitane data z mouse
+    signal plus_x, plus_y : std_logic_vector(10 downto 0);
+
+    signal btn_l, btn_l_pom             : std_logic;
+    signal btn_r, btn_r_pom             : std_logic;
+    signal btn_m, btn_m_pom             : std_logic;
+    signal x_data                       : std_logic_vector(8 downto 0);
+    signal y_data                       : std_logic_vector(8 downto 0);
+    signal pos_x_next, pos_x, pos_x_pom : std_logic_vector(10 downto 0);
+    signal pos_y_next, pos_y, pos_y_pom : std_logic_vector(10 downto 0);
 
     component debouncer is
         port(clk, ps2c : in  STD_LOGIC;
@@ -75,21 +72,20 @@ architecture Behavioral of MOUSE_top is
     end component;
 
     component state_log_m is
-		port(clk, rst                     : in  STD_LOGIC;
-         tx_done_en, rx_done_en       : in  STD_LOGIC;
-         rx_data                      : in  STD_LOGIC_VECTOR(7 downto 0);
-         tx_en, rx_en, CE_rdy                 : out STD_LOGIC;
-         num_to_mouse                 : out STD_LOGIC_VECTOR(7 downto 0);
-         btn_l_out, btn_r_out, btn_m_out : out STD_LOGIC;
-			x_pretec_out, y_pretec_out		: out STD_LOGIC;
-         x_data_out                   : out STD_LOGIC_VECTOR(8 downto 0);
-         y_data_out                   : out STD_LOGIC_VECTOR(8 downto 0));
-	end component;
-	
-	component scitac_11bit is
-    Port ( a, b : in  STD_LOGIC_VECTOR (10 downto 0);
-           y : out  STD_LOGIC_VECTOR (10 downto 0));
-	end component;
+        port(clk, rst                        : in  STD_LOGIC;
+             tx_done_en, rx_done_en          : in  STD_LOGIC;
+             rx_data                         : in  STD_LOGIC_VECTOR(7 downto 0);
+             tx_en, rx_en, CE_rdy            : out STD_LOGIC;
+             num_to_mouse                    : out STD_LOGIC_VECTOR(7 downto 0);
+             btn_l_out, btn_r_out, btn_m_out : out STD_LOGIC;
+             x_data_out                      : out STD_LOGIC_VECTOR(8 downto 0);
+             y_data_out                      : out STD_LOGIC_VECTOR(8 downto 0));
+    end component;
+
+    component scitac_11bit is
+        Port(a, b : in  STD_LOGIC_VECTOR(10 downto 0);
+             y    : out STD_LOGIC_VECTOR(10 downto 0));
+    end component;
 
 begin
     ps2c_iobuf : IOBUF
@@ -108,69 +104,85 @@ begin
                  ps2c_t_en    => T_ENABLE_cl, ps2c_in => OUT_SIG_cl, ps2d_t_en => T_ENABLE_dat, ps2d_in => OUT_SIG_dat, tx_done_en => tx_done_en);
 
     citanie_z_mouse : ps2_rx
-        port map(ps2c_fall  => ps2c_fall, ps2d => IN_SIG_dat, clk => clk, rst => rst, rx_en => rx_en, ps2d_data => rx_data,
-                 rx_done_en => rx_done_en);
+        port map(clk        => clk,
+                 rst        => rst,
+                 ps2d       => IN_SIG_dat,
+                 ps2c_fall  => ps2c_fall,
+                 rx_en      => rx_en,
+                 rx_done_en => rx_done_en,
+                 ps2d_data  => rx_data);
 
     hlavny_stav_automat : state_log_m
-        port map(clk          => clk, rst => rst, tx_done_en => tx_done_en, rx_done_en => rx_done_en, rx_data => rx_data, tx_en => tx_en, rx_en => rx_en,
-                 btn_l_out     => btn_l, btn_r_out => btn_r, btn_m_out => btn_m, x_data_out  => x_data, y_data_out => y_data, 
-					  num_to_mouse => num_to_mouse, CE_rdy => CE_rdy, x_pretec_out => x_pretec, y_pretec_out => y_pretec);
-					  
-	--position decoder x
-process (x_data)
-begin
-	if (x_data(8) = '1') then
-		plus_x <= "11" & x_data(8 downto 0);
-	else 
-		plus_x <= "00" & x_data(8 downto 0);
-	end if;
-end process;
+        port map(clk          => clk,
+                 rst          => rst,
+                 tx_done_en   => tx_done_en,
+                 rx_done_en   => rx_done_en,
+                 rx_data      => rx_data,
+                 tx_en        => tx_en,
+                 rx_en        => rx_en,
+                 CE_rdy       => CE_rdy,
+                 num_to_mouse => num_to_mouse,
+                 btn_l_out    => btn_l,
+                 btn_r_out    => btn_r,
+                 btn_m_out    => btn_m,
+                 x_data_out   => x_data,
+                 y_data_out   => y_data);
 
---position decoder y
-process (y_data)
-begin	
-	if (y_data(8) = '0') then
-		plus_y <= "11" & not (y_data(8 downto 0));
-	else 
-		plus_y <= "00" & not (y_data(8 downto 0));
-	end if;
-end process;
+    --position decoder x
+    process(x_data)
+    begin
+        if (x_data(8) = '1') then
+            plus_x <= "11" & x_data(8 downto 0);
+        else
+            plus_x <= "00" & x_data(8 downto 0);
+        end if;
+    end process;
 
-scitanie_pos_x: scitac_11bit
-		port map (a => pos_x_next, b => plus_x, y => pos_x_pom);
+    --position decoder y
+    process(y_data)
+    begin
+        if (y_data(8) = '0') then
+            plus_y <= "11" & not (y_data(8 downto 0));
+        else
+            plus_y <= "00" & not (y_data(8 downto 0));
+        end if;
+    end process;
 
-scitanie_pos_y: scitac_11bit
-		port map (a => pos_y_next, b => plus_y, y => pos_y_pom);
-		
---synchronna pozice		
-process (clk, rst, CE_rdy)
-	begin
-		if (rst = '1') then
-			pos_x_next <= "01001111111";
-			pos_y_next <= "00111111111";
-			btn_r_pom <= '0';
-			btn_l_pom <= '0';
-			btn_m_pom <= '0';
-		elsif(clk'event and clk='1') then
-			if ( CE_rdy = '1') then
-				pos_x_next <= pos_x;
-				pos_y_next <= pos_y;
-				btn_r_pom <= btn_r;
-				btn_l_pom <= btn_l;
-				btn_m_pom <= btn_m;
-			end if;
-		end if;end process;
+    scitanie_pos_x : scitac_11bit
+        port map(a => pos_x_next, b => plus_x, y => pos_x_pom);
 
---pretecenie x mimo obraz
-pos_x <= "10011111111" when unsigned(pos_x_pom) > "10011111111" else pos_x_pom;
---osetrenie y posledneho bitu
-pos_y <= "01111111111" when unsigned(pos_y_pom) > "01111111111" else pos_y_pom;
+    scitanie_pos_y : scitac_11bit
+        port map(a => pos_y_next, b => plus_y, y => pos_y_pom);
 
-position_x <= pos_x_next;
-position_y <= pos_y_next(9 downto 0);
-button_l <= btn_l_pom;
-button_r <= btn_r_pom;
-button_m <= btn_m_pom;
+    --synchronna pozice		
+    process(clk, rst)
+    begin
+        if (rst = '1') then
+            pos_x_next <= "01001111111";
+            pos_y_next <= "00111111111";
+            btn_r_pom  <= '0';
+            btn_l_pom  <= '0';
+            btn_m_pom  <= '0';
+        elsif (clk'event and clk = '1') then
+            if (CE_rdy = '1') then
+                pos_x_next <= pos_x;
+                pos_y_next <= pos_y;
+                btn_r_pom  <= btn_r;
+                btn_l_pom  <= btn_l;
+                btn_m_pom  <= btn_m;
+            end if;
+        end if;
+    end process;
 
+    --pretecenie x mimo obraz
+    pos_x <= "10011111111" when unsigned(pos_x_pom) > "10011111111" else pos_x_pom;
+    --osetrenie y posledneho bitu
+    pos_y <= "01111111111" when unsigned(pos_y_pom) > "01111111111" else pos_y_pom;
+
+    position_x <= pos_x_next;
+    position_y <= pos_y_next(9 downto 0);
+    button_l   <= btn_l_pom;
+    button_r   <= btn_r_pom;
+    button_m   <= btn_m_pom;
 
 end Behavioral;
